bc2ccea86cab7958509e58774984fbe9130d4b21
swapneethpunna <swapneethpunna@msitprogram.net>
2021-09-29 10:59:08 +0530

    implemented  drawGrid function

--- battleship.py
+++ battleship.py
@@ -44,8 +44,8 @@ Parameters: dict mapping strs to values ; Tkinter canvas ; Tkinter canvas
 Returns: None
 '''
 def makeView(data, userCanvas, compCanvas):
-     #userCanvas=drawGrid(data, userCanvas, data["user_board"],True)
-     #compCanvas=drawGrid(data,compCanvas,data["comp_board"],True)
+    userCanvas=drawGrid(data, userCanvas, data["user_board"],True)
+    compCanvas=drawGrid(data,compCanvas,data["comp_board"],True)
     return
 
 
